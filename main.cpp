4a00e5bee11170cccfaeaeec45a9a0bcf08e26dd
Babii Victor264 <victor-ionut.babii@s.unibuc.ro>
2024-10-16 23:51:16 +0300

    teste

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
-#include<SFML/Graphics.hpp>
+// #include<SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow game(sf::VideoMode(800,600),"Game");
-    while(game.isOpen()) {
-        sf::Event event;
-        while (game.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                game.close();
-        }
-        game.clear(sf::Color::Black);
-        game.display();
-    }
+    // sf::RenderWindow game(sf::VideoMode(800,600),"Game");
+    // while(game.isOpen()) {
+    //     sf::Event event;
+    //     while (game.pollEvent(event)) {
+    //         if (event.type == sf::Event::Closed)
+    //             game.close();
+    //     }
+    //     game.clear(sf::Color::Black);
+    //     game.display();
+    // }
 return 0;
 }
